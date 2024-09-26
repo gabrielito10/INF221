4f77137586478c1aa619489cdf3e77d139c09d46
gabrielito10 <j.gabito@gmail.com>
2024-09-26 18:01:11 -0400

    INF221

---
+++ RepasoArray/src/ArraysDos.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ArraysDos {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		System.out.println("Digite longitud --> ");
+		int N = x.nextInt();
+		int[] numeros = new int[N];
+		for (int i=0 ; i < N ; i++)
+		{
+			System.out.println("Valor en la posicion " + i);
+			numeros[i] = x.nextInt();
+		}
+		Arrays.sort(numeros);
+		// imprmiendo el array ordenado
+		for (int i=0 ; i < N ; i++)
+		{
+			System.out.println(numeros[i]);
+		}
+	}
+
+}
