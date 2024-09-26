a9695b26457a5cb275b0e9f921b1791ee71120ef
gabrielito10 <j.gabito@gmail.com>
2024-09-26 19:51:20 -0400

    INF221

--- Recursividad/src/SumaNumeros.java
+++ Recursividad/src/SumaNumeros.java
@@ -1,9 +1,18 @@
-
+import java.util.*;
 public class SumaNumeros {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es una prueba");
-
+		Scanner x = new Scanner(System.in);
+		System.out.println("Digite valor de N");
+		int N = x.nextInt();
+		System.out.println(suma(N));
+	}
+	static int suma(int n)
+	{
+		if (n == 1)
+			return 1;
+		else
+			return  n + suma(n - 1);
 	}
 
 }
